0db8c1b5717734786d1506ebd5b83e4474323e9c
Carlito Algarin <carlito1113@yahoo.com>
2020-11-09 18:31:00 -0500

    added in more text to the footer and adjusted its placement on the page to the right

--- carlito-portfolio/src/components/Footer.js
+++ carlito-portfolio/src/components/Footer.js
@@ -8,10 +8,13 @@ function Footer() {
     
     return(
         <footer className="mt-5">
-            <Container>
-                <Row>
+            <Container fluid={true}>
+                <Row className="border-top justify-content-between p-3">
                     <Col className="p-0" md={3} sm={12}>
                         Carlito Algarin
+                    </Col>
+                    <Col className="p-0 d-flex justify-content-end" md={3}>
+                        This site was created by Carlito Algarin
                     </Col>
                 </Row>
             </Container>
