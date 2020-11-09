e72fb58b32fb9c2eb01ba1b2beba0e3a2157051a
Carlito Algarin <carlito1113@yahoo.com>
2020-11-09 15:47:06 -0500

    added portfolio title, and an array of objects for the links in the header. Home, About, and Contact

--- carlito-portfolio/src/App.js
+++ carlito-portfolio/src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 
 class App extends React.Component {
 
+  // 
   constructor(props) {
     super(props);
     this.state = {
+      title: 'Carlito Algarin',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' },
+      ],
+
       
     }
   }
 
+    // displays content to the page
   render() {
     return (
       <div>Hello from React</div>
