1452386e7b89a70b8c2e7075fb5675abd7027d87
Carlito Algarin <carlito1113@yahoo.com>
2020-11-10 16:16:16 -0500

    added additional links and object to carousel

--- carlito-portfolio/src/components/Carousel.js
+++ carlito-portfolio/src/components/Carousel.js
@@ -30,6 +30,30 @@ class Carousel extends React.Component {
                         link: 'https://damp-falls-85688.herokuapp.com/',
                         selected: false
                     },
+                    {
+                        id: 3,
+                        title: 'Fitness Tracker',
+                        subTitle: 'An application to create, store, and update workouts',
+                        imgSrc: fitnesstracker,
+                        link: 'https://infinite-journey-49679.herokuapp.com/?id=5f9cb775dedcff0017199027',
+                        selected: false
+                    },
+                    {
+                        id: 4,
+                        title: 'Note Taker',
+                        subTitle: 'An application to create, store, and delete notes',
+                        imgSrc: notetaker,
+                        link: 'https://notetakerforcarlitoalgarin.herokuapp.com/',
+                        selected: false
+                    },
+                    {
+                        id: 5,
+                        title: 'Burger Time',
+                        subTitle: 'A simple application to add, update, and delete burgers',
+                        imgSrc: burgertime,
+                        link: 'https://arcane-wildwood-34618.herokuapp.com/',
+                        selected: false
+                    },
                 ]
             }
         }
