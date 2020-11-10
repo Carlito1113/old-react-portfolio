04df505dee849847a4d666e190334d8603a5cda4
Carlito Algarin <carlito1113@yahoo.com>
2020-11-10 17:47:25 -0500

    changed onClick to click to allow clicked project pictures to display their links and sub-text

--- carlito-portfolio/src/components/Card.js
+++ carlito-portfolio/src/components/Card.js
@@ -5,7 +5,7 @@ import CardInfo from '../components/CardInfo';
 function Card(props) {
 
     return(
-        <div className="d-inline-block g-card">
+        <div className="d-inline-block g-card" onClick={() => props.click(props.item)}>
             <img className="g-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
             { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} />  }
         </div>
