d73015a626a20e249efb07370ad2dff8df2c4f1a
Carlito Algarin <carlito1113@yahoo.com>
2020-11-10 15:54:11 -0500

    added in an array of objects into the constructor

--- carlito-portfolio/src/components/Carousel.js
+++ carlito-portfolio/src/components/Carousel.js
@@ -5,7 +5,32 @@ class Carousel extends React.Component {
         constructor(props){
             super(props);
             this.state = {
-
+                items: [
+                    {
+                        id: 0,
+                        title: 'Wine&Dines',
+                        subTitle: 'Recipe generator & wine pairing app',
+                        imgSrc: wineanddines,
+                        link: '',
+                        selected: false
+                    },
+                    {
+                        id: 1,
+                        title: 'Audiophyle',
+                        subTitle: 'Application for music lovers',
+                        imgSrc: audiophyle,
+                        link: '',
+                        selected: false
+                    },
+                    {
+                        id: 2,
+                        title: '',
+                        subTitle: '',
+                        imgSrc: ,
+                        link: '',
+                        selected: false
+                    },
+                ]
             }
         }
 
