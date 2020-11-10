7fb41ec552a8c0b3cee763b265e75a537393f396
Carlito Algarin <carlito1113@yahoo.com>
2020-11-10 18:27:35 -0500

    added in text to the about me page

--- carlito-portfolio/src/pages/AboutPage.js
+++ carlito-portfolio/src/pages/AboutPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Content from '../components/Content';
 
 function AboutPage(props) {
     
     return(
-        <p>AboutPage Functional!</p>
-    );
+        <div>
+            <Hero title={props.title} />
+
+            <Content>
+                <p>Hello, my name is Carlito. I am a full stack engineer with experience in MERN stack.</p>
+                <p>I wish to learn and grow with a passionate company, develop my skills to the highest degree possible, and create my own projects of passion.</p>
+                <p>I am a calm and focused individual. When I am not studying, I am playing guitar, reading, or building something.</p>
+            </Content>
+        </div>
+        );
 }
 
 export default AboutPage;
