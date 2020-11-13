41bd151cbb049b2cb63ecd608cb05cef5c5fe41d
Carlito Algarin <carlito1113@yahoo.com>
2020-11-13 13:57:07 -0500

    created a second carousel for the 4th, 5th, and 6th projects because 6 in a single carousel looked too compact

--- carlito-portfolio/src/components/Carousel2.js
+++ carlito-portfolio/src/components/Carousel2.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import Card from '../components/Card';
 
-import wineanddines from '../assets/images/wineanddines.png';
-import audiophyle from '../assets/images/audiophyle.png';
-import budgettracker from '../assets/images/budgettracker.png';
+import fitnesstracker from '../assets/images/fitnesstracker.png';
+import notetaker from '../assets/images/notetaker.png';
+import burgertime from '../assets/images/burgertime.png';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
-class Carousel extends React.Component {
+class Carousel2 extends React.Component {
 
         constructor(props){
             super(props);
@@ -17,26 +17,26 @@ class Carousel extends React.Component {
                 items: [
                     {
                         id: 0,
-                        title: 'Wine&Dines',
-                        subTitle: 'Recipe generator & wine pairing app',
-                        imgSrc: wineanddines,
-                        link: 'https://andrewrb22.github.io/-.theFederationOfCoders-/',
+                        title: 'Fitness Tracker',
+                        subTitle: 'An application to create, store, and update workouts',
+                        imgSrc: fitnesstracker,
+                        link: 'https://infinite-journey-49679.herokuapp.com/?id=5f9cb775dedcff0017199027',
                         selected: false
                     },
                     {
                         id: 1,
-                        title: 'Audiophyle',
-                        subTitle: 'Application for music lovers',
-                        imgSrc: audiophyle,
-                        link: 'https://audiophyle-music-platform.herokuapp.com/',
+                        title: 'Note Taker',
+                        subTitle: 'An application to create, store, and delete notes',
+                        imgSrc: notetaker,
+                        link: 'https://notetakerforcarlitoalgarin.herokuapp.com/',
                         selected: false
                     },
                     {
                         id: 2,
-                        title: 'Budget Tracker',
-                        subTitle: 'An application for budget calculation',
-                        imgSrc: budgettracker,
-                        link: 'https://damp-falls-85688.herokuapp.com/',
+                        title: 'Burger Time',
+                        subTitle: 'A simple application to add, update, and delete burgers',
+                        imgSrc: burgertime,
+                        link: 'https://arcane-wildwood-34618.herokuapp.com/',
                         selected: false
                     },
                 ]
@@ -79,4 +79,4 @@ class Carousel extends React.Component {
 }
 
 
-export default Carousel;
+export default Carousel2;
--- carlito-portfolio/src/pages/HomePage.js
+++ carlito-portfolio/src/pages/HomePage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Carousel from '../components/Carousel';
 import Hero from '../components/Hero';
+import Carousel2 from '../components/Carousel2';
+
 
 function HomePage(props) {
 
@@ -8,6 +10,7 @@ function HomePage(props) {
         <div>
             <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
             <Carousel />
+            <Carousel2 />
         </div>
     );
 }
