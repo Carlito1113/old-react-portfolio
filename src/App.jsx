ce513330c5c83b93c14c69417b22da3ffb6707fe
Carlito Algarin <carlito1113@yahoo.com>
2021-07-02 18:24:31 -0400

    added in hamburger menu functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import { useState } from 'react';
 import './app.scss';
+import Menu from './components/menu/Menu';
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
