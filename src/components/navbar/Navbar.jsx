c97646d153c92f88c2c16ca96544391b95c1dd66
Carlito Algarin <carlito1113@yahoo.com>
2021-07-02 14:54:54 -0400

    added in material ui icons for contacts

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,22 @@
 import './navbar.scss';
+import PersonIcon from '@material-ui/icons/Person';
+import MailIcon from '@material-ui/icons/Mail';
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className='logo'>genius.</a>
+          <a href="#intro" className="logo">
+            genius.
+          </a>
           <div className="itemContainer">
-            
+            <PersonIcon className="icon" />
+            <span>+727 331 0237</span>
+          </div>
+          <div className="itemContainer">
+            <MailIcon className="icon" />
+            <span>carlito1113@yahoo.com</span>
           </div>
         </div>
         <div className="right"></div>
