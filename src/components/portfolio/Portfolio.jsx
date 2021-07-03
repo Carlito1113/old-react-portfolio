de77b9904bbd4f96fc9c6c2df3e568f7c86f0125
Carlito Algarin <carlito1113@yahoo.com>
2021-07-03 12:16:12 -0400

    added in dynamic active class changes to the portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,14 +1,45 @@
+import { useState, useEffect } from 'react';
 import './portfolio.scss';
+import PortfolioList from '../portfolioList/PortfolioList';
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState('featured');
+  const list = [
+    {
+      id: 'featured',
+      title: 'Featured',
+    },
+    {
+      id: 'web',
+      title: 'Web App',
+    },
+    {
+      id: 'mobile',
+      title: 'Mobile App',
+    },
+    {
+      id: 'design',
+      title: 'Design',
+    },
+    {
+      id: 'content',
+      title: 'Content',
+    },
+  ];
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        <li className="active">Featured</li>
-        <li>Recommend</li>
-        <li>Recipebook</li>
-        <li>Audiophyle</li>
+        {list.map(item => {
+          return (
+            <PortfolioList
+              title={item.title}
+              active={selected === item.id}
+              setSelected={setSelected}
+              id={item.id}
+            />
+          );
+        })}
       </ul>
 
       <div className="container">
