cf39744cc7a996812532cb40bad931602429770b
Carlito Algarin <carlito1113@yahoo.com>
2021-07-03 11:47:38 -0400

    created portfolio section and added in dummy projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,42 @@
-import './portfolio.scss'
+import './portfolio.scss';
 
 export default function Portfolio() {
   return (
-    <div className='portfolio' id='portfolio'>
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Recommend</li>
+        <li>Recipebook</li>
+        <li>Audiophyle</li>
+      </ul>
 
+      <div className="container">
+        <div className="item">
+          <img src="assets/recommend.png" alt="Recommend book app" />
+          <h3>Recommend</h3>
+        </div>
+        <div className="item">
+          <img src="assets/recommend.png" alt="Recommend book app" />
+          <h3>Recommend</h3>
+        </div>
+        <div className="item">
+          <img src="assets/recommend.png" alt="Recommend book app" />
+          <h3>Recommend</h3>
+        </div>
+        <div className="item">
+          <img src="assets/recommend.png" alt="Recommend book app" />
+          <h3>Recommend</h3>
+        </div>
+        <div className="item">
+          <img src="assets/recommend.png" alt="Recommend book app" />
+          <h3>Recommend</h3>
+        </div>
+        <div className="item">
+          <img src="assets/recommend.png" alt="Recommend book app" />
+          <h3>Recommend</h3>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
