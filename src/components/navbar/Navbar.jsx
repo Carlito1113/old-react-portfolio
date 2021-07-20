f636644404cb70fae30c17d67ad22298d1e5f642
Carlito Algarin <carlito1113@yahoo.com>
2021-07-20 19:06:22 -0400

    styling and addition of icons to the intro page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import MailIcon from '@material-ui/icons/Mail';
 
 export default function Navbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className={"navbar " + (menuOpen && 'active')}>
+    <div className={'navbar ' + (menuOpen && 'active')}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-          {`</>`}
+            {`{C.A}`}
           </a>
           <div className="itemContainer">
             <PersonIcon className="icon" />
@@ -17,6 +17,26 @@ export default function Navbar({ menuOpen, setMenuOpen }) {
           <div className="itemContainer">
             <MailIcon className="icon" />
             <span>carlito1113@yahoo.com</span>
+          </div>
+          <div className="itemContainer">
+            <a
+              href="https://github.com/Carlito1113"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-github icon"></i>
+            </a>
+          </div>
+          <div className="itemContainer">
+            <span>
+              <a
+                href="https://www.linkedin.com/in/carlito-algarin-5b29771b1/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fab fa-linkedin-in icon"></i>
+              </a>
+            </span>
           </div>
         </div>
         <div className="right">
